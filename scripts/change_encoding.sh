a587212ecdbdf43378621bfa1c3d1d1aac183dfa
luizsantos <luiz.arthur.feitosa.santos@gmail.com>
2013-08-29 00:18:03 -0300

    primeiro

---
+++ scripts/change_encoding.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+fcode="$1"
+tcode="$2"
+
+for arq in $(find . -name '*.tex')
+do
+    fcode=$(file --mime-encoding $arq | awk '{print $2;}')
+    echo "$arq: $fcode"
+    if [ $fcode == $1 ]
+    then
+       $(cp $arq $arq.bak)
+       $(iconv -f $fcode -t $tcode $arq -o $arq)
+    fi
+done
+
+echo "FIM"
+
+exit 0
+ 
